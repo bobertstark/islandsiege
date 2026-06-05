fffecce1f35c7990c8f93f5770d5cb2847fdfe6d
bobert <bobertstark@users.noreply.github.com>
2026-06-04 20:47:29 -0400

    fix: ensure player attributes are reset when ending

--- src/common/handlers/endTurn.ts
+++ src/common/handlers/endTurn.ts
@@ -2,5 +2,13 @@ import IGameState from 'common/IGameState'
 
 export function handleEndTurn(state: IGameState): IGameState {
   const next = (state.currentPlayerIndex + 1) % state.players.length
-  return { ...state, currentPlayerIndex: next, phase: 'victory' }
+  return {
+    ...state,
+    currentPlayerIndex: next,
+    phase: 'victory',
+    attackRoll: undefined,
+    attackRerollsRemaining: 0,
+    diceBank: {},
+    attackIsOpenWater: false,
+  }
 }
